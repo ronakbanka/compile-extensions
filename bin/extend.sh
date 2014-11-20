69ded5156fa94bff0560ab045df80a1888ab4141
Jacques Chester, Rick Reilly and Sai To Yeung <pair+jchester+rreilly+syeung@pivotal.io>
2014-11-20 17:25:59 -0500

    Add path shimming to extend.sh
    [#83076478]

--- bin/extend.sh
+++ bin/extend.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-echo "-------> Buildpack version $(cat $1/VERSION)"
+set -euo pipefail
+
+BUILDPACK_PATH=$1
+
+echo "-------> Buildpack version $(cat $BUILDPACK_PATH/VERSION)"
+
+if test -d $BUILDPACK_PATH/dependencies; then
+  export PATH=$BUILDPACK_PATH/compile-extensions/bin:$PATH
+fi
