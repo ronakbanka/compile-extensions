15ab3358c2e96f9b437412ba9cf0bf5d01d20c7a
Rasheed Abdul-Aziz and Sai To Yeung <pair+squeedee+syeung@pivotal.io>
2014-11-24 10:33:17 -0500

    Use idiomatic strings

--- spec/curl_spec.rb
+++ spec/curl_spec.rb
@@ -1,11 +1,11 @@
-require "spec_helper"
-require "open3"
-require "tempfile"
+require 'spec_helper'
+require 'open3'
+require 'tempfile'
 
-describe "curl" do
+describe 'curl' do
   attr :stdout, :stderr, :status
 
-  CURL_CMD = File.join(File.dirname(__FILE__), "..", "bin", "curl")
+  CURL_CMD = File.join(File.dirname(__FILE__), '..', 'bin', 'curl')
 
   def curl argstring
     shell_out "#{CURL_CMD} #{argstring}"
@@ -15,28 +15,27 @@ describe "curl" do
     @stdout, @stderr, @status = Open3.capture3 command
   end
 
+  context 'when BUILDPACK_PATH is not set' do
+    before { ENV.delete('BUILDPACK_PATH') }
 
-  context "when BUILDPACK_PATH is not set" do
-    before { ENV.delete("BUILDPACK_PATH") }
+    it 'emits a warning message' do
+      curl 'http://files.com/file.txt'
 
-    it "emits a warning message" do
-      curl "http://files.com/file.txt"
-
-      expect(stderr).to include("You are running a buildpack version of curl but have not set BUILDPACK_PATH")
+      expect(stderr).to include('You are running a buildpack version of curl but have not set BUILDPACK_PATH')
     end
 
-    it "exits with non-zero status code" do
-      curl "http://files.com/file.txt"
+    it 'exits with non-zero status code' do
+      curl 'http://files.com/file.txt'
 
       expect(status).to_not be_success
     end
   end
 
-  context "when BUILDPACK_PATH is set" do
-    let(:buildpack_path) { "/tmp/common_spec" }
+  context 'when BUILDPACK_PATH is set' do
+    let(:buildpack_path) { '/tmp/common_spec' }
 
     before do
-      ENV["BUILDPACK_PATH"] = buildpack_path
+      ENV['BUILDPACK_PATH'] = buildpack_path
       FileUtils.mkdir_p buildpack_path
     end
 
@@ -44,67 +43,67 @@ describe "curl" do
       FileUtils.rm_rf buildpack_path
     end
 
-    it "warns when the file does not exist as a dependency" do
-      expected_output="Resource http://files.com/file.txt is not provided by this buildpack. Please upgrade your buildpack to receive the latest resources."
+    it 'warns when the file does not exist as a dependency' do
+      expected_output='Resource http://files.com/file.txt is not provided by this buildpack. Please upgrade your buildpack to receive the latest resources.'
 
-      curl "http://files.com/file.txt"
+      curl 'http://files.com/file.txt'
       expect(stderr).to include(expected_output)
     end
 
-    context "a file is in the cache" do
-      let(:protocol) { "http" }
+    context 'a file is in the cache' do
+      let(:protocol) { 'http' }
 
       before do
-        dep_dir = File.join(buildpack_path, "dependencies")
+        dep_dir = File.join(buildpack_path, 'dependencies')
         file_name = "#{protocol}___files.com_file.txt"
         cached_file = File.join(dep_dir, file_name)
         FileUtils.mkdir_p dep_dir
-        File.open(cached_file, "w") { |f| f.write "some text" }
+        File.open(cached_file, 'w') { |f| f.write 'some text' }
       end
 
       it "grabs the cached file's contents" do
         curl "#{protocol}://files.com/file.txt"
 
-        expect(stdout).to eq("some text")
+        expect(stdout).to eq('some text')
       end
 
-      context "curl is invoked with output parameter" do
-        context "which is stdout" do
+      context 'curl is invoked with output parameter' do
+        context 'which is stdout' do
           it "grabs the cached file's contents" do
             curl "-o- #{protocol}://files.com/file.txt"
 
-            expect(stdout).to eq("some text")
+            expect(stdout).to eq('some text')
           end
         end
 
-        context "which is another file" do
+        context 'which is another file' do
           it "grabs the cached file's contents" do
-            filename = Tempfile.new("cache").path
+            filename = Tempfile.new('cache').path
             begin
               curl "-o #{filename} #{protocol}://files.com/file.txt"
 
-              expect(File.read(filename)).to eq("some text")
+              expect(File.read(filename)).to eq('some text')
             ensure
               FileUtils.rm filename
             end
           end
         end
 
-        context "which is the original file name" do
+        context 'which is the original file name' do
           it "puts the cached file's contents into a file in the PWD" do
             curl "-sO #{protocol}://files.com/file.txt"
-            expect(File.read("file.txt")).to eq("some text")
-            FileUtils.rm "file.txt"
+            expect(File.read('file.txt')).to eq('some text')
+            FileUtils.rm 'file.txt'
           end
         end
 
-        context "https" do
-          let!(:protocol) { "https" }
+        context 'https' do
+          let!(:protocol) { 'https' }
 
           it "grabs the file's contents" do
-            curl "https://files.com/file.txt"
+            curl 'https://files.com/file.txt'
 
-            expect(stdout).to eq("some text")
+            expect(stdout).to eq('some text')
           end
         end
       end
