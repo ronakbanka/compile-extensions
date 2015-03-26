9c140ed007a14ba4ec83d7a45a155a576edd868f
Anthony Emengo and JT Archie <pair+aemengo+jarchie@pivotal.io>
2015-03-25 20:19:38 -0400

    WIP: refactor lib/dependencies
    - Attempts to make the intentions more readily visible to the user
    - Currently untested

--- lib/dependencies.rb
+++ lib/dependencies.rb
@@ -1,58 +1,51 @@
 module CompileExtensions
   class Dependencies
+
     def initialize(manifest)
       @manifest = manifest
     end
 
     def find_matching_dependency(uri)
-      mapping = find_dependency_mapping(uri)
-
-      return nil if mapping.nil?
-
-      mapping = transform_mapping_values(mapping, uri)
+      mapping = find_url_to_dependency_mapping(uri)
       find_dependency_with_mapping(mapping)
     end
 
     def find_translated_url(uri)
-      dependency = find_matching_dependency(uri)
-
-      return nil if dependency.nil?
-
-      dependency['uri']
+      if dependency = find_matching_dependency(uri)
+        dependency['uri']
+      end
     end
 
     private
 
-    def transform_mapping_values(mapping, uri)
-      matches = uri.match(mapping['match'])
-      %w{name version}.each do |key|
-        if matches.length > 1
-          (1...matches.length).each do |index|
-            mapping[key].gsub!("$#{index}", matches[index])
-
-          end
-        end
-      end
-      mapping
-    end
-
-    def find_dependency_mapping(uri)
+    def find_url_to_dependency_mapping(uri)
       @manifest['url_to_dependency_map'].find do |mapping|
-        uri.match(mapping['match'])
-      end
+        matches = uri.match(mapping['match'])
+        next unless matches.length > 1
 
+        new_mapping            = Hash.new
+        new_mapping['name']    = extract_versions_from_matchdata(mapping['name'], matches)
+        new_mapping['version'] = extract_versions_from_matchdata(mapping['version'], matches)
+        new_mapping
+      end
     end
 
     def find_dependency_with_mapping(mapping)
+      return nil unless mapping
+
       @manifest['dependencies'].find do |dependency|
         dependency['version'].to_s == mapping['version'].to_s &&
           dependency['name'] == mapping['name'] &&
-          dependency_satisfies_current_stack(dependency)
+          dependency['cf_stacks'].include?(stack)
       end
     end
 
-    def dependency_satisfies_current_stack(dependency)
-      dependency['cf_stacks'].include?(stack)
+    def extract_versions_from_matchdata(mapping_value, matches)
+      if mapping_value_matches = mapping_value.match(%r"\$d")
+        matches[mapping_value_matches.first]
+      else
+        mapping_value
+      end
     end
 
     def stack
